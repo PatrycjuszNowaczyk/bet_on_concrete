50a956b5d1801bd153c458553afc5d705a1bd47f
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-20 16:09:14 +0200

    install tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div>This is your app</div>;
+  return (
+    <div className="bg-gray-500 text-white text-center p-8">
+      This is your app
+    </div>
+  );
 }
 
 export default App;
