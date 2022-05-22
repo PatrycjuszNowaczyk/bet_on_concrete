31ec73d85e65f7973a36009c277dc604e1e95f0e
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-23 01:54:00 +0200

    add new page components

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+export default function Contact() {
+  return (
+    <div>
+    <h1>
+    Contact
+    </h1>
+    <Link className="mx-1" to="/">Home</Link>
+    </div>
+  )
+}
---
+++ src/pages/Shop/ShopItem/ShopItem.jsx
@@ -0,0 +1,5 @@
+export default function ShopItem() {
+  return (
+    <div>ShopItem</div>
+  )
+}
