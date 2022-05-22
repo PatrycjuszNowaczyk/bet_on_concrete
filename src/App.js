f97a901bc1c5b60aa5b82daea017473aae19610a
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-23 01:54:38 +0200

    implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
+import About from "./pages/About/About";
+import Shop from "./pages/Shop/Shop";
+import ShopCategory from "./pages/Shop/ShopCategory/ShopCategory";
+import Contact from "./pages/Contact/Contact";
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/shop">
+        <Route index element={<Shop />} />
+        <Route path=":categoryId" element={<ShopCategory />} />
+      </Route>
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<div>404</div>} />
+    </Routes>
   );
 }
 
