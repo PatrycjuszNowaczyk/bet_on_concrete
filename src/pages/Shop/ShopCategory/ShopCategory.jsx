0237080713403cbb1ce81b4619a14a85277f48fa
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-23 21:51:46 +0200

    test link to 404

--- src/pages/Shop/ShopCategory/ShopCategory.jsx
+++ src/pages/Shop/ShopCategory/ShopCategory.jsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
 export default function ShopCategory() {
-  return <div>ShopCategory</div>;
+  return (
+    <div>
+      ShopCategory
+      <p>
+        <Link to="/sadf">Unknown</Link>
+      </p>
+    </div>
+  );
 }
