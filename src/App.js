f3e4aebb8387dd91df1a915d600223f76a476031
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-23 21:48:41 +0200

    move imports to specific file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage/Homepage";
-import About from "./pages/About/About";
-import Shop from "./pages/Shop/Shop";
-import ShopCategory from "./pages/Shop/ShopCategory/ShopCategory";
-import Contact from "./pages/Contact/Contact";
-import O_404 from "./pages/404/O_404";
+// prettier-ignore
+import { React, Routes, Route, Homepage, About, Shop, ShopCategory, Contact, O_404 } from './appImports.js';
 function App() {
   return (
     <Routes>
