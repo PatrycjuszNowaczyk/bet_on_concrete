04cc201562deceded817be205c4d5da77f8ade91
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-23 21:49:57 +0200

    change view of 404

--- src/pages/404/O_404.jsx
+++ src/pages/404/O_404.jsx
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function O_404() {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-center items-center bg-cyan-900 h-[100vh]">
       <p className="text-9xl text-center text-white">404</p>
-      <Link className="mx-1 text-white" to="..">I'll be back mthrfckrs</Link>
+      <button
+        className="ml-8 p-2 text-white border-2 border-white hover:bg-blue-300 hover:border-blue-300"
+        onClick={() => navigate(-1)}
+      >
+        go back
+      </button>
     </div>
   );
 }
