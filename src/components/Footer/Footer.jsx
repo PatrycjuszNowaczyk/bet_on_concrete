e324ac5ff80f6bd430e48c83eadd4893648095ee
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-24 02:22:17 +0200

    change footer to stick to the bottom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 export default function Footer() {
   return (
-    <div className="text-center bg-neutral-500 text-white text-2xl">Footer</div>
+    <div className="text-center bg-neutral-500 text-white text-2xl py-4">Footer</div>
   )
 }
