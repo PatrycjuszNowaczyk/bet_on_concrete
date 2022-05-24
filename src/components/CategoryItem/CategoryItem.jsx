2e979e27020a9ee3dbbff9b96f527efc32533773
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-24 02:21:57 +0200

    add transition on hover

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -6,7 +6,7 @@ export default function CategoryItem(props) {
       className="w-[30%] p-4 sm:p-12 grow min-w-[150px] sm:min-w-[245px] border-2 border-black flex justify-center items-center cursor-pointer group bg-cover aspect-square sm:aspect-video"
       style={{ backgroundImage: `url(${imgUrl})` }}
     >
-      <div className="p-2 sm:p-4 border-2 border-black bg-white bg-opacity-80 group-hover:bg-neutral-500 group-hover:text-white flex flex-wrap items-center grow-0">
+      <div className="p-2 sm:p-4 border-2 border-black bg-white bg-opacity-80 transition-colors duration-500 group-hover:transition-colors group-hover:duration-200 group-hover:bg-neutral-500 group-hover:text-white flex flex-wrap items-center grow-0">
         <div>
           <h2 className="uppercase text-2xl text-center font-bold">
             {title}
