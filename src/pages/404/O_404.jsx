45e494e710853222559c3874dbcde345e3ec57fd
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-25 19:38:21 +0200

    small changes in 404

--- src/pages/404/O_404.jsx
+++ src/pages/404/O_404.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from "react-router-dom";
 export default function O_404() {
   const navigate = useNavigate();
   return (
-    <div className="flex justify-center items-center bg-cyan-900 h-[100vh]">
+    <div className="flex flex-1 justify-center items-center bg-cyan-900 h-[100vh]">
       <p className="text-9xl text-center text-white">404</p>
       <button
-        className="ml-8 p-2 text-white border-2 border-white hover:bg-blue-300 hover:border-blue-300"
+        className="ml-8 p-2 text-white border-2 border-white hover:bg-neutral-500"
         onClick={() => navigate(-1)}
       >
         go back
