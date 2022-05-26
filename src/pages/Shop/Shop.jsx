f1d97e39f03de22608a27ab2531b63a3214ce199
PatrycjuszNowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-27 01:31:55 +0200

    change shopdata

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -8,39 +8,58 @@ export default class Shop extends Component {
     this.state = ShopData;
   }
   render() {
-    console.log(this.state);
-    const { items } = this.state;
+    const { categories } = this.state;
     return (
       <>
         <div className="text-center text-4xl bg-black py-2 mb-8 text-white">
           Shop
         </div>
-        <div className="container mx-auto flex items-start">
+        <div className="container px-2 mx-auto flex items-start">
           <div className="w-1/3 lg:w-1/4 xl:w-1/5 inline-block">
             <ShopFilters />
           </div>
-          <div className="inline-block w-4/5">
-            <div className="p-2 border-2 ml-8 border-black grid gap-4 grid-cols-3 xl:grid-cols-4">
-              {items.map((item) => {
-                return (
-                  <div
-                    className="flex items-end relative p-2 bg-neutral-200 hover:bg-black transition-colors duration-300 hover:duration-1000 basis-1/4 aspect-square group"
-                    key={item.id}
-                  >
-                    <div
-                      className="absolute flex inset-2 group-hover:inset-1 transition-all duration-300 group-hover:duration-400 bg-cover bg-center"
-                      style={{ backgroundImage: `url(${item.mainImg})` }}
-                    ></div>
-                    <div className="relative flex flex-wrap justify-end bg-opacity-75 grow">
-                      <p className="text-sm md:text-base lg:text-xl leading-tight lg:leading-normal text-center bg-white grow">
-                        <strong>{item.name}</strong>
-                      </p>
-                      <p className="px-1 text-lg lg:text-2xl text-right bg-white border-2 border-black">
-                        {item.price} zł
-                      </p>
+          <div className="w-2/3 lg:w-3/4 xl:w-4/5">
+            <div className="p-2 border-2 ml-8 border-black">
+              {categories.map((category) => {
+                if (category.items.length > 0) {
+                  return (
+                    <div key={category.id}>
+                      <h2
+                        className={`text-2xl md:text-3xl xl:text-4xl font-bold uppercase ${
+                          category.id > 1 ? "pt-8" : "pt-4"
+                        } pb-4`}
+                      >
+                        {category.name}
+                      </h2>
+                      <div className="grid gap-4 grid-cols-3 xl:grid-cols-4">
+                        {category.items.map((item) => {
+                          return (
+                            <div
+                              className="flex items-end relative p-2 hover:bg-black border-2 border-black transition-colors duration-300 hover:duration-1000 basis-1/4 aspect-square group"
+                              key={item.id}
+                            >
+                              <div
+                                className="absolute flex inset-2 group-hover:inset-1 transition-all duration-300 group-hover:duration-400 bg-cover bg-center"
+                                style={{
+                                  backgroundImage: `url(${item.mainImg})`,
+                                }}
+                              ></div>
+                              <div className="relative flex flex-wrap justify-end bg-opacity-75 grow">
+                                <p className="text-sm md:text-base lg:text-xl leading-tight lg:leading-normal text-center bg-white grow">
+                                  <strong>{item.name}</strong>
+                                </p>
+                                <p className="px-1 text-lg lg:text-2xl text-right bg-white border-2 border-black">
+                                  {item.price} zł
+                                </p>
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
                     </div>
-                  </div>
-                );
+                  );
+                }
+                return null;
               })}
               ;
             </div>
--- src/pages/Shop/shopData.js
+++ src/pages/Shop/shopData.js
@@ -1,79 +1,136 @@
 let shopData = {
-  items: [
+  categories: [
     {
       id: 1,
-      name: "eco gryzak skórzany",
-      price: "10",
-      producer: "Animini",
-      mainImg: "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
-      carouselImgs: [
-        "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
-        "https://cf.shopee.pl/file/57020a53ce5be73c05560178a4563f64",
-        "https://cf.shopee.pl/file/a873d77a44fdfa8d4f53801d48e03f3f",
+      name: "gryzaki",
+      items: [
+        {
+          id: 11,
+          name: "eco gryzak skórzany",
+          price: 10,
+          producer: "Animini",
+          mainImg: "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
+          carouselImgs: [
+            "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
+            "https://cf.shopee.pl/file/57020a53ce5be73c05560178a4563f64",
+            "https://cf.shopee.pl/file/a873d77a44fdfa8d4f53801d48e03f3f",
+          ],
+          description: "To jest opis gryzaka",
+          availability: "dostępny",
+        },
+        {
+          id: 12,
+          name: "poroże jelenia",
+          price: 17,
+          producer: "nat4dog",
+          mainImg: "https://nat4dog.pl/userdata/public/gfx/1257/188cfd6d90775ade234ffd6a8f1624f3.jpg",
+          carouselImgs: [
+            "https://4dogsoriginal.com/wp-content/uploads/2021/04/gryzak-z-poroza-jelenia-rozmiar-S-hard-012_web.jpg",
+            "https://4dogsoriginal.com/wp-content/uploads/2021/04/poroze-jelenia-dla-psa-rozmiar-M-hard-02.jpg",
+            "https://4dogsoriginal.com/wp-content/uploads/2021/04/gryzak-z-poroza-jelenia_web2-1.jpg",
+            "https://4dogsoriginal.com/wp-content/uploads/2021/04/infografika-zalety-porozejelenia-hard.jpg",
+          ],
+          description: "Poroże ze zrzutu jelenia jako gryzak dla psa",
+          availability: "dostępny",
+        }
       ],
-      description: "To jest opis gryzaka",
-      category: "gryzaki",
-      availability: "dostępny",
     },
     {
       id: 2,
-      name: "eco przysmak zielony",
-      price: "21",
-      producer: "Animini",
-      mainImg: "https://image.ceneostatic.pl/data/products/53075392/i-chico-przysmak-kabanosy-zielone-kielbaski-100szt.jpg",
-      carouselImgs: [
-        "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
-        "https://cf.shopee.pl/file/57020a53ce5be73c05560178a4563f64",
-        "https://cf.shopee.pl/file/a873d77a44fdfa8d4f53801d48e03f3f",
+      name: "przysmaki",
+      items: [
+        {
+          id: 21,
+          name: "eco przysmak zielony",
+          price: 21,
+          producer: "Animini",
+          mainImg:
+            "https://image.ceneostatic.pl/data/products/53075392/i-chico-przysmak-kabanosy-zielone-kielbaski-100szt.jpg",
+          carouselImgs: [
+            "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
+            "https://cf.shopee.pl/file/57020a53ce5be73c05560178a4563f64",
+            "https://cf.shopee.pl/file/a873d77a44fdfa8d4f53801d48e03f3f",
+          ],
+          description: "To jest opis przysmaku",
+          availability: "dostępny",
+        },
       ],
-      description: "To jest opis przysmaku",
-      category: "przysmaki",
-      availability: "dostępny",
     },
     {
       id: 3,
-      name: "suplement na stawy",
-      price: "34",
-      producer: "Doggy",
-      mainImg: "https://fish4dogspolska.pl/wp-content/uploads/2020/05/suplement-diety-dla-psa-fish4dogs-stawy.jpg",
-      carouselImgs: [
-        "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
-        "https://cf.shopee.pl/file/57020a53ce5be73c05560178a4563f64",
-        "https://cf.shopee.pl/file/a873d77a44fdfa8d4f53801d48e03f3f",
+      name: "suplementy",
+      items: [
+        {
+          id: 31,
+          name: "suplement na stawy",
+          price: 34,
+          producer: "Doggy",
+          mainImg:
+            "https://fish4dogspolska.pl/wp-content/uploads/2020/05/suplement-diety-dla-psa-fish4dogs-stawy.jpg",
+          carouselImgs: [
+            "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
+            "https://cf.shopee.pl/file/57020a53ce5be73c05560178a4563f64",
+            "https://cf.shopee.pl/file/a873d77a44fdfa8d4f53801d48e03f3f",
+          ],
+          description: "To jest opis przysmaku",
+          availability: "dostępny",
+        },
       ],
-      description: "To jest opis przysmaku",
-      category: "suplementy",
-      availability: "dostępny",
     },
     {
       id: 4,
-      name: "eco kosmetyk do psa",
-      price: "97",
-      producer: "PetLi",
-      mainImg: "https://megazoologiczny.pl/pol_pm_Over-zoo-silk-spray-250ml-19145_1.png",
-      carouselImgs: [
-        "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
-        "https://cf.shopee.pl/file/57020a53ce5be73c05560178a4563f64",
-        "https://cf.shopee.pl/file/a873d77a44fdfa8d4f53801d48e03f3f",
+      name: "kosmetyki",
+      items: [
+        {
+          id: 41,
+          name: "eco kosmetyk do psa",
+          price: 97,
+          producer: "PetLi",
+          mainImg:
+            "https://megazoologiczny.pl/pol_pm_Over-zoo-silk-spray-250ml-19145_1.png",
+          carouselImgs: [
+            "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
+            "https://cf.shopee.pl/file/57020a53ce5be73c05560178a4563f64",
+            "https://cf.shopee.pl/file/a873d77a44fdfa8d4f53801d48e03f3f",
+          ],
+          description: "To jest opis kosmetyku do głowy dla psa",
+          availability: "dostępny",
+        },
       ],
-      description: "To jest opis kosmetyku do głowy dla psa",
-      category: "kosmetyki",
-      availability: "dostępny",
     },
     {
       id: 5,
-      name: "kaganiec super eco",
-      price: "125",
-      producer: "Choppy",
-      mainImg: "https://groomershop.pl/media/catalog/product/thumbnail/64bd19accd0b8c98a3b672933bf160cf/image/0/500x500/111/80/0/6/06d5264ce22d4ad6edccfbcad9459fb9_7.jpg",
-      carouselImgs: [
-        "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
-        "https://cf.shopee.pl/file/57020a53ce5be73c05560178a4563f64",
-        "https://cf.shopee.pl/file/a873d77a44fdfa8d4f53801d48e03f3f",
+      name: "akcesoria",
+      items: [
+        {
+          id: 51,
+          name: "kaganiec super eco",
+          price: 125,
+          producer: "Choppy",
+          mainImg:
+            "https://groomershop.pl/media/catalog/product/thumbnail/64bd19accd0b8c98a3b672933bf160cf/image/0/500x500/111/80/0/6/06d5264ce22d4ad6edccfbcad9459fb9_7.jpg",
+          carouselImgs: [
+            "https://cf.shopee.pl/file/ae169f5d030b92a90e5e53992a04e8ab",
+            "https://cf.shopee.pl/file/57020a53ce5be73c05560178a4563f64",
+            "https://cf.shopee.pl/file/a873d77a44fdfa8d4f53801d48e03f3f",
+          ],
+          description: "To jest opis akcesorium dla psa",
+          availability: "na zamówienie",
+        },
+        {
+          id: 52,
+          name: "obroża eco",
+          price: 150,
+          producer: "Choppy",
+          mainImg:
+            "https://4wild.eu/userdata/public/gfx/139/3d2fe21e93c4a58bf9f5060849f30767.jpg",
+          carouselImgs: [
+            "https://4wild.eu/userdata/public/gfx/139/3d2fe21e93c4a58bf9f5060849f30767.jpg",
+            "https://4wild.eu/userdata/public/gfx/140/1beb0b272b67cc7160f3952710ad0793.jpg",
+            "https://4wild.eu/userdata/public/gfx/141/0822b385317c2625e78aca7403389919.jpg",
+          ],
+        },
       ],
-      description: "To jest opis akcesorium dla psa",
-      category: "akcesoria",
-      availability: "na zamówienie",
     },
   ],
 };
