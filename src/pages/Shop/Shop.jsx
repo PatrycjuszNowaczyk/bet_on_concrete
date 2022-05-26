c198c2bbfafeaa1f266b1a0b4348c67fbe4be4c9
PatrycjuszNowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-26 17:22:05 +0200

    display shop items

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -9,12 +9,43 @@ export default class Shop extends Component {
   }
   render() {
     console.log(this.state);
+    const { items } = this.state;
     return (
       <>
-        <div className="text-center text-4xl bg-black py-2 mb-8 text-white">Shop</div>
-        <div className="container mx-auto">
-          <div className="w-1/5 pr-4">
+        <div className="text-center text-4xl bg-black py-2 mb-8 text-white">
+          Shop
+        </div>
+        <div className="container mx-auto flex items-start">
+          <div className="w-1/3 lg:w-1/4 xl:w-1/5 inline-block">
             <ShopFilters />
+          </div>
+          <div className="inline-block w-4/5">
+            <div className="p-2 border-2 ml-8 border-black grid gap-4 grid-cols-3 xl:grid-cols-4">
+              {items.map((item) => {
+                return (
+                  <div
+                    className="p-2 border-2 border-black rounded-lg basis-1/4 aspect-square"
+                    key={item.id}
+                  >
+                    <div className="flex flex-row">
+                      <div className="w-1/3">
+                        <img
+                          src={item.mainImg}
+                          alt="mainImg"
+                          className="w-full"
+                        />
+                      </div>
+                      <div className="w-2/3">
+                        <div className="text-xl">{item.name}</div>
+                        <div className="text-lg">{item.producer}</div>
+                        <div className="text-base">{item.description}</div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+              ;
+            </div>
           </div>
         </div>
       </>
