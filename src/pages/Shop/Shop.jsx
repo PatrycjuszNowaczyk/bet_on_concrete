c961cb627d2fce8330f215ad55c4fbcbd84f53ec
PatrycjuszNowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-05-26 03:27:11 +0200

    add ShopFilter component to Shop component

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,5 +1,13 @@
+import ShopFilters from "../../components/ShopFilters/ShopFilters";
 export default function Shop() {
   return (
-    <div className="text-center text-4xl bg-slate-800 text-white">Shop</div>
-  )
+    <>
+      <div className="text-center text-4xl bg-slate-800 text-white">Shop</div>
+      <div className="container mx-auto">
+        <div className="w-1/5 pr-4">
+          <ShopFilters />
+        </div>
+      </div>
+    </>
+  );
 }
